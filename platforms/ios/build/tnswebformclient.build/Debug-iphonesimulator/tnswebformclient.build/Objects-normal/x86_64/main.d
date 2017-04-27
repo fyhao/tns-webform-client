2d729b28ab437a547cb0d76c28abcc0d28602b76
fyhao <fyhao1@gmail.com>
2017-04-27 08:20:22 -0700

    init tns

---
+++ platforms/ios/build/tnswebformclient.build/Debug-iphonesimulator/tnswebformclient.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,8 @@
+dependencies: \
+  /Users/user903758/git/tns-webform-client/platforms/ios/internal/main.m \
+  /Users/user903758/git/tns-webform-client/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/user903758/git/tns-webform-client/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/user903758/git/tns-webform-client/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/user903758/git/tns-webform-client/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/user903758/git/tns-webform-client/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/user903758/git/tns-webform-client/platforms/ios/internal/TNSDebugging.h
