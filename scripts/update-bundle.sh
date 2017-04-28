33ccaa1f53a6df44dcff68bea6d344a5296f7e40
fyhao <fyhao1@gmail.com>
2017-04-29 01:41:14 +0800

    move above update-bundle

--- scripts/update-bundle.sh
+++ scripts/update-bundle.sh
@@ -9,6 +9,6 @@ if [[ "$TRAVIS_BRANCH" != "staging" ]]; then
 fi
 
 PROVISIONING_PROFILE="$HOME/Library/MobileDevice/Provisioning Profiles/$PROFILE_NAME.mobileprovision"
-OUTPUTDIR="$PWD/build/Release-iphoneos"
+#OUTPUTDIR="$PWD/build/Release-iphoneos"
 
-xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTPUTDIR/$APP_NAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
+#xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTPUTDIR/$APP_NAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
