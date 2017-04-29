9892fc0e13959a06cd6b0b7e553db781b8206172
fyhao <fyhao1@gmail.com>
2017-04-28 20:28:22 -0700

    add Info.plist build.xcconfig to App_resources

---
+++ platforms/ios/build/tnswebformclient.build/Release-iphoneos/tnswebformclient.build/Objects-normal/arm64/main.d
@@ -0,0 +1,7 @@
+dependencies: \
+  /Users/user903758/git/tns-webform-client/platforms/ios/internal/main.m \
+  /Users/user903758/git/tns-webform-client/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/user903758/git/tns-webform-client/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/user903758/git/tns-webform-client/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/user903758/git/tns-webform-client/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/user903758/git/tns-webform-client/platforms/ios/internal/TNSExceptionHandler.h
