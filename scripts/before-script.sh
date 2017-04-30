02e42019913099111932346a80fa9f5e91ada9f9
fyhao <fyhao1@gmail.com>
2017-04-30 23:32:56 +0800

    semicolon

--- scripts/before-script.sh
+++ scripts/before-script.sh
@@ -4,12 +4,12 @@ echo "Inside before-script"
 ## Here put a switch for ad hoc and app store distribution
 ## 1 = ad hoc, 2 = app store
 PROVISIONING_TYPE = "2"
-if test "$PROVISIONING_TYPE" = '1' then
+if test "$PROVISIONING_TYPE" = '1'; then
 	PROFILE_NAME="tnswebformprofile"
 	PROVISIONING_PROFILE="b96261dd-8691-46d4-89f4-1cfb195e10a3"
 fi
 
-if test "$PROVISIONING_TYPE" = '2' then
+if test "$PROVISIONING_TYPE" = '2'; then
 	PROFILE_NAME="tnswebformappstore"
 	PROVISIONING_PROFILE="986f708a-ebbf-4d8c-afda-4e5f82c934c4"
 fi
