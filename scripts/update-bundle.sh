c41ecb70666617aba7672e67ae897c9194429441
fyhao <fyhao1@gmail.com>
2017-04-30 22:45:58 +0800

    remove debug

--- scripts/update-bundle.sh
+++ scripts/update-bundle.sh
@@ -8,19 +8,7 @@ if [[ "$TRAVIS_BRANCH" != "staging" ]]; then
   exit 0
 fi
 
-
-echo "DEBUG update-bundle.sh TRAVIS_PULL_REQUEST $TRAVIS_PULL_REQUEST"
-echo "DEBUG update-bundle.sh TRAVIS_BRANCH $TRAVIS_BRANCH"
-echo "DEBUG update-bundle.sh PROFILE_NAME $PROFILE_NAME"
-echo "DEBUG update-bundle.sh Check folder platforms/ios/build/device"
-ls -la /Users/travis/build/fyhao/tns-webform-client/platforms/ios/build/device
-echo "DEBUG update-bundle.sh Check folder platforms/ios/build/sharedpch"
-ls -la /Users/travis/build/fyhao/tns-webform-client/platforms/ios/build/sharedpch
-
 PROVISIONING_PROFILE="~/Library/MobileDevice/Provisioning\ Profiles/$PROFILE_NAME.mobileprovision"
 OUTPUTDIR="platforms/ios/build/emulator"
 
 ##xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTPUTDIR/$APP_NAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
-
-echo "DEBUG update-bundle.sh after xcrun check folder"
-ls -la "$OUTPUTDIR"
