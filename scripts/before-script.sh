4488220beb918b87afad4c8e6b45e1d729d027d7
fyhao <fyhao1@gmail.com>
2017-04-30 23:26:03 +0800

    test appstore

--- scripts/before-script.sh
+++ scripts/before-script.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 echo "Inside before-script"
-openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in scripts/profile/tnswebformprofile.mobileprovision.enc -d -a -out scripts/profile/tnswebformprofile.mobileprovision
+
+## Here put a switch for ad hoc and app store distribution
+## 1 = ad hoc, 2 = app store
+PROVISIONING_TYPE = "2"
+if test "$PROVISIONING_TYPE" = '1' then
+	PROFILE_NAME="tnswebformprofile"
+	PROVISIONING_PROFILE="b96261dd-8691-46d4-89f4-1cfb195e10a3"
+fi
+
+if test "$PROVISIONING_TYPE" = '2' then
+	PROFILE_NAME="tnswebformappstore"
+	PROVISIONING_PROFILE="986f708a-ebbf-4d8c-afda-4e5f82c934c4"
+fi
+
+openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in scripts/profile/$PROFILE_NAME.mobileprovision.enc -d -a -out scripts/profile/$PROFILE_NAME.mobileprovision
 openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in scripts/certs/dist.cer.enc -d -a -out scripts/certs/dist.cer
 openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in scripts/certs/dist.p12.enc -d -a -out scripts/certs/dist.p12
 
