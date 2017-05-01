25c8749d6e646a03fc8bc9bfb6da297f9ad10753
fyhao <fyhao1@gmail.com>
2017-05-02 00:42:23 +0800

    APP_FOLDER

--- scripts/after-script.sh
+++ scripts/after-script.sh
@@ -8,24 +8,25 @@ PROVISIONING_TYPE="${e[2]}"
 
 ./scripts/remove-key.sh
 
-ls -la /Users/travis/build/fyhao/tns-webform-client/platforms/ios/build/device/
-
-
 
 APP_PATH="$APP_NAME.ipa"
 if test "$PROVISIONING_TYPE" = '0'; then
 	APP_PATH="$APP_NAME.app"
 fi
 
+APP_FOLDER="/Users/travis/build/fyhao/tns-webform-client/platforms/ios/build/device/"
 ## TO_ZIP_FILE: 0 = .ipa file, 1 = .zip file
 TO_ZIP_FILE="1" 
 
 if test "$TO_ZIP_FILE" = '1'; then
-	zip -r "$APP_NAME.zip" . -i "$APP_NAME*"
+	zip -r "${APP_FOLDER}$APP_NAME.zip" "${APP_FOLDER}$APP_NAME*"
 	APP_PATH="$APP_NAME.zip"
 fi
 
 
 echo "APP_PATH=$APP_PATH"
 
-curl -u $SAUCE_USERNAME:$SAUCE_ACCESS_KEY -X POST -H "Content-Type: application/octet-stream" https://saucelabs.com/rest/v1/storage/$SAUCE_USERNAME/$APP_PATH?overwrite=true --data-binary "@/Users/travis/build/fyhao/tns-webform-client/platforms/ios/build/device/$APP_PATH"
+ls -la ${APP_FOLDER}
+
+
+curl -u $SAUCE_USERNAME:$SAUCE_ACCESS_KEY -X POST -H "Content-Type: application/octet-stream" https://saucelabs.com/rest/v1/storage/$SAUCE_USERNAME/$APP_PATH?overwrite=true --data-binary "@${APP_FOLDER}$APP_PATH"
