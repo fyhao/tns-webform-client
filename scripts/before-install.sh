eb4896a60626e8fa929924701c5def4459107bc5
fyhao <fyhao1@gmail.com>
2017-05-01 22:47:46 +0800

    move provisioning type switch into before-install

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,17 +1,46 @@
 #!/bin/sh
 echo "Inside before-install"
+
+## Here put a switch for ad hoc and app store distribution
+## 1 = ad hoc, 2 = app store, 0 = debugsimulator
+PROVISIONING_TYPE="1"
+if test "$PROVISIONING_TYPE" = '1'; then
+	PROFILE_NAME="tnswebformprofile"
+	PROVISIONING_PROFILE="b96261dd-8691-46d4-89f4-1cfb195e10a3"
+fi
+
+if test "$PROVISIONING_TYPE" = '2'; then
+	PROFILE_NAME="tnswebformappstore"
+	PROVISIONING_PROFILE="986f708a-ebbf-4d8c-afda-4e5f82c934c4"
+fi
+
+declare -a e
+e[0]="$PROFILE_NAME"
+e[1]="$PROVISIONING_PROFILE"
+e[2]="$PROVISIONING_TYPE"
+echo "${e[@]}" > /tmp/tempout
+
+
+
+
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
 source ~/.nvm/nvm.sh && nvm install $NODE_VERSION && nvm use $NODE_VERSION
 PATH="`npm bin`:`npm bin -g`:$PATH"
 brew update > /dev/null;
 npm install -g npm@$NPM_VERSION
-npm install grunt-cli -g
-git clone https://github.com/fyhao/nativescript-cli
-cd nativescript-cli
-git submodule update --init
-npm install
-grunt
-cd ../
+
+if test "$PROVISIONING_TYPE" = '0'; then
+	npm install nativescript@$TNS_VERSION
+else
+	npm install grunt-cli -g
+	git clone https://github.com/fyhao/nativescript-cli
+	cd nativescript-cli
+	git submodule update --init
+	npm install
+	grunt
+	cd ../
+fi
+
 npm install
   # CocoaPods
 gem install cocoapods --pre --no-rdoc --no-ri --no-document --quiet
@@ -27,5 +56,3 @@ xcpretty --version
 xcodebuild -version
 xcodebuild -showsdks
 
-
-echo "inside before install PROVISIONING_PROFILE = $PROVISIONING_PROFILE"
--- scripts/before-script.sh
+++ scripts/before-script.sh
@@ -1,25 +1,10 @@
 #!/bin/sh
 echo "Inside before-script"
 
-## Here put a switch for ad hoc and app store distribution
-## 1 = ad hoc, 2 = app store, 0 = debugsimulator
-PROVISIONING_TYPE="1"
-if test "$PROVISIONING_TYPE" = '1'; then
-	PROFILE_NAME="tnswebformprofile"
-	PROVISIONING_PROFILE="b96261dd-8691-46d4-89f4-1cfb195e10a3"
-fi
 
-if test "$PROVISIONING_TYPE" = '2'; then
-	PROFILE_NAME="tnswebformappstore"
-	PROVISIONING_PROFILE="986f708a-ebbf-4d8c-afda-4e5f82c934c4"
-fi
-
-declare -a e
-e[0]="$PROFILE_NAME"
-e[1]="$PROVISIONING_PROFILE"
-e[2]="$PROVISIONING_TYPE"
-echo "${e[@]}" > /tmp/tempout
-
+read -r -a e < /tmp/tempout
+PROFILE_NAME="${e[0]}"
+PROVISIONING_PROFILE="${e[1]}"
 echo "inside before script PROVISIONING_PROFILE = $PROVISIONING_PROFILE"
 openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in scripts/profile/$PROFILE_NAME.mobileprovision.enc -d -a -out scripts/profile/$PROFILE_NAME.mobileprovision
 openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in scripts/certs/dist.cer.enc -d -a -out scripts/certs/dist.cer
