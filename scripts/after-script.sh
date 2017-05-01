a183ae2a07584e7f07bf8b1329cbbbb0be4527eb
fyhao <fyhao1@gmail.com>
2017-05-01 14:05:33 +0800

    move upload saucelabs into after-script

--- scripts/after-script.sh
+++ scripts/after-script.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 echo "Inside after-script"
 ./scripts/remove-key.sh
+
+ls -la /Users/travis/build/fyhao/tns-webform-client/platforms/ios/build/device/
+curl -u $SAUCE_USERNAME:$SAUCE_ACCESS_KEY -X POST -H "Content-Type: application/octet-stream" https://saucelabs.com/rest/v1/storage/$SAUCE_USERNAME/$APP_NAME?overwrite=true --data-binary "@/Users/travis/build/fyhao/tns-webform-client/platforms/ios/build/device/tnswebformclient.ipa"
