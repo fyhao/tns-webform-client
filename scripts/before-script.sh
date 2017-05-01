766d9fd5c12f6338d390c8cdcf95e93b47ead9b0
fyhao <fyhao1@gmail.com>
2017-05-01 17:16:46 +0800

    Update before-script.sh type 1

--- scripts/before-script.sh
+++ scripts/before-script.sh
@@ -3,7 +3,7 @@ echo "Inside before-script"
 
 ## Here put a switch for ad hoc and app store distribution
 ## 1 = ad hoc, 2 = app store, 0 = debugsimulator
-PROVISIONING_TYPE="0"
+PROVISIONING_TYPE="1"
 if test "$PROVISIONING_TYPE" = '1'; then
 	PROFILE_NAME="tnswebformprofile"
 	PROVISIONING_PROFILE="b96261dd-8691-46d4-89f4-1cfb195e10a3"
