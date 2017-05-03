2117b14d2fbba876b404cf22c85c32586b2f8297
fyhao <fyhao1@gmail.com>
2017-05-03 23:03:36 +0800

    init unit test

---
+++ test/alltests.js
@@ -0,0 +1,2 @@
+
+require('./testTemp.js');
---
+++ test/testTemp.js
@@ -0,0 +1,15 @@
+var path = require('path');
+var assert = require('assert');
+global.ProjRequire = function(module) {
+	return require(path.join(__dirname, '/../' + module)); 
+}
+
+describe('test', function() {
+  this.timeout(15000);
+  
+  describe('test', function() {
+	it('should able to test', function(done) {
+		done();
+    });
+  });
+});
