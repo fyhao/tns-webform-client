eca4999f4229c60007a93d0c22744465dc65c900
fyhao <fyhao1@gmail.com>
2017-05-03 23:12:23 +0800

    npm coverage if branch dev and integration

--- scripts/after-success.sh
+++ scripts/after-success.sh
@@ -4,3 +4,11 @@ if test "$TRAVIS_BRANCH" = 'staging'; then
 	./scripts/update-bundle.sh
 	./scripts/sign-and-upload.sh
 fi
+
+if test "$TRAVIS_BRANCH" = 'dev'; then
+	npm run coverage
+fi
+
+if test "$TRAVIS_BRANCH" = 'integration'; then
+	npm run coverage
+fi
