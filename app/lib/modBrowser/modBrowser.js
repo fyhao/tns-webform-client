18feef3ea9b12ac07e773acc7dd5537139e7c81e
fyhao <fyhao1@gmail.com>
2017-05-09 06:56:14 -0700

    fix helpers error on modBrowser.js

--- app/lib/modBrowser/modBrowser.js
+++ app/lib/modBrowser/modBrowser.js
@@ -1,9 +1,13 @@
+var helpers = require('../../utils/widgets/helper');
 module.exports.createBrowser = function() {
 	return new Browser();
 }
 
 function Browser() {
 	this.open = function(url) {
-		
+		helpers.navigate({
+			moduleName : 'components/browserView/browserView',
+			context : {url:url}
+		});
 	}
 }
