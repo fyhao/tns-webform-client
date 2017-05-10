5957587ac6a28a7daff80f02060e0cf6bd1fb56b
fyhao <fyhao1@gmail.com>
2017-05-10 02:38:48 -0700

    current comment out set clipboard and created a bug under #50

--- app/lib/modBrowser/modBrowser.js
+++ app/lib/modBrowser/modBrowser.js
@@ -392,9 +392,20 @@ var FlowEngine = function(flow) {
 			setTimeout(next, 1);
 		}
 		else if(step.type == 'setclipboard') {
-			clipboard.setText(step.message).then(function() {
-				setTimeout(next, 1);
-			});
+			/*
+			var utils = require("utils/utils");
+			console.log(1);
+			var pasteboard = utils.ios.getter(UIPasteboard, UIPasteboard.generalPasteboard);
+		      console.log(2);
+		      try {
+		      	pasteboard.setValueForPasteboardType(step.message, kUTTypePlainText);
+		      } catch (e) {
+		      	console.log('some error detected')
+		      }
+		      
+		      console.log(3);
+		      */
+		      setTimeout(next, 1);
 		}
 		else if(step.type == 'wait') {
 			setTimeout(next, step.timeout);
--- app/utils/nativeClipboard.js
+++ app/utils/nativeClipboard.js
@@ -4,9 +4,7 @@ var utils = require("utils/utils");
 exports.setText = function (content) {
   return new Promise(function (resolve, reject) {
     try {
-      var pasteboard = utils.ios.getter(UIPasteboard, UIPasteboard.generalPasteboard);
-      pasteboard.setValueForPasteboardType(content, kUTTypePlainText);
-      resolve();
+      
     } catch (ex) {
       console.log("Error in clipboard.setText: " + ex);
       reject(ex);
