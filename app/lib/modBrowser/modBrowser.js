fc5629342f35f1b365fd8d0ae54c121ae3d16c99
fyhao <fyhao1@gmail.com>
2017-05-22 23:20:11 +0800

    #59 move clone function to util

--- app/lib/modBrowser/modBrowser.js
+++ app/lib/modBrowser/modBrowser.js
@@ -384,7 +384,7 @@ var FlowEngine = function(flow) {
 		item = v;
 		return this;
 	}
-	this.flow = clone(flow);
+	this.flow = util.clone(flow);
 	this.canceled = false;
 	var vars = {};
 	this.execute = function(done) {
@@ -606,46 +606,3 @@ var FlowEngine = function(flow) {
 	}
 }
 
-var clone = function clone(item) {
-    if (!item) { return item; } // null, undefined values check
-
-    var types = [ Number, String, Boolean ], 
-        result;
-
-    // normalizing primitives if someone did new String('aaa'), or new Number('444');
-    types.forEach(function(type) {
-        if (item instanceof type) {
-            result = type( item );
-        }
-    });
-
-    if (typeof result === "undefined") {
-        if (Object.prototype.toString.call( item ) === "[object Array]") {
-            result = [];
-            item.forEach(function(child, index, array) { 
-                result[index] = clone( child );
-            });
-        } else if (typeof item === "object") {
-            // testing that this is DOM
-            if (item.nodeType && typeof item.cloneNode === "function") {
-                item.cloneNode( true );    
-            } else if (!item.prototype) { // check that this is a literal
-                if (item instanceof Date) {
-                    result = new Date(item);
-                } else {
-                    // it is an object literal
-                    result = {};
-                    for (var i in item) {
-                        result[i] = clone( item[i] );
-                    }
-                }
-            } else {
-                result = item;
-            }
-        } else {
-            result = item;
-        }
-    }
-
-    return result;
-}
