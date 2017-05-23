e9697c0d23f4cb18544ef24011b72da3d87a8fe2
fyhao <fyhao1@gmail.com>
2017-05-23 23:13:50 +0800

    #47 FlowEngine requestFlow step level

--- app/lib/modBrowser/modBrowser.js
+++ app/lib/modBrowser/modBrowser.js
@@ -546,6 +546,32 @@ var FlowEngine = function(flow) {
 		else if(step.type == 'requestFlow') {
 			step.callbackJSON = function(json) {
 				new FlowEngine(json.flow).setItem(item).setWv(wv).execute(next);
+				//#47 request flow step level
+				if(typeof json.flows != 'undefined') {
+					for(var i in json.flows) {
+						ctx.flows[i] = json.flows[i];
+					}
+				}
+	
+				var flow = json.flow;
+				if(typeof flow != 'undefined') {
+					if(typeof flow == 'object') {
+						// flow object
+						new FlowEngine(flow).setContext(ctx).execute(next);
+					}
+					else if(typeof flow == 'string') {
+						// flow name
+						if(typeof ctx.flows[flow] != 'undefined') {
+							new FlowEngine(ctx.flows[flow]).setContext(ctx).execute(next);
+						}
+					}
+					else {
+						setTimeout(next, 1);
+					}
+				}
+				else {
+					setTimeout(next, 1);
+				}
 			}
 			util.frequest(step);
 		}
