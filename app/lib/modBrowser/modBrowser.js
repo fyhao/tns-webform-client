77e3ad2550f8f94d3d2914eb122320cb92ee91c1
fyhao <fyhao1@gmail.com>
2017-05-23 23:13:11 +0800

    #47 FlowEngine submit callback level

--- app/lib/modBrowser/modBrowser.js
+++ app/lib/modBrowser/modBrowser.js
@@ -324,6 +324,27 @@ var handleChange = function(widgetName) {
 				}
 				if(json.flow) {
 					new FlowEngine(json.flow).setItem(item).setWv(wv).execute(function() {});
+				
+				//#47 submit callback level
+				
+				if(typeof json.flows != 'undefined') {
+					for(var i in json.flows) {
+						ctx.flows[i] = json.flows[i];
+					}
+				}
+	
+				var flow = json.flow;
+				if(typeof flow != 'undefined') {
+					if(typeof flow == 'object') {
+						// flow object
+						new FlowEngine(flow).setContext(ctx).execute(function() {});
+					}
+					else if(typeof flow == 'string') {
+						// flow name
+						if(typeof ctx.flows[flow] != 'undefined') {
+							new FlowEngine(ctx.flows[flow]).setContext(ctx).execute(function() {});
+						}
+					}
 				}
 			}
 		});
