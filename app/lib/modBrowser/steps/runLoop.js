092b8020c90c27acfeda94c767d8789193690fef
fyhao <fyhao1@gmail.com>
2017-05-27 19:53:56 +0800

    #96 runLoop

---
+++ app/lib/modBrowser/steps/runLoop.js
@@ -0,0 +1,85 @@
+module.exports = {
+	
+	process : function(ctx, step, next) {
+		if(typeof step.end != 'undefined') {
+			processLoop(ctx, step, next);
+		}
+		else if(typeof step.array != 'undefined') {
+			processArray(ctx, step, next);
+		}
+		else {
+			setTimeout(next, 1);
+		}
+	}
+}
+
+var processLoop = function(ctx, step, next) {
+	var start = 0;
+	if(typeof step.start != 'undefined') start = parseInt(step.start);
+	var end = parseInt(step.end);
+	var step = 1;
+	if(typeof step.step != 'undefined') step = parseInt(step.step);
+	var i = start; // for start
+	var checkNext = function() {
+		if(i < end) { // for end condition
+			var tempFlow = null;
+			if(typeof ctx.flows != 'undefined') {
+				tempFlow = ctx.flows[step.flow];
+			}
+			var inputVars = {};
+			for(var i in ctx._vars) {
+				inputVars[i] = ctx._vars[i];
+			}
+			inputVars[itemName] = array[i];
+			ctx.createFlowEngine(tempFlow).setInputVars(inputVars).execute(function(outputVars) {
+				if(typeof outputVars != 'undefined') {
+					for(var i in outputVars) {
+						ctx._vars[i] = outputVars[i];
+					}
+				}
+				i += step; // for step
+				setTimeout(checkNext, 1);
+			});
+		}
+		else {
+			setTimeout(next, 1);
+		}
+	}
+	setTimeout(checkNext, 1);
+}
+
+var processArray = function(ctx, step, next) {
+	var array = ctx.vars[step.array];
+	if(array && array.length) {
+		var itemName = typeof step.item !== 'undefined' ? step.item : 'item';
+		var i = 0;
+		var checkNext = function() {
+			if(i < array.length) {
+				var tempFlow = null;
+				if(typeof ctx.flows != 'undefined') {
+					tempFlow = ctx.flows[step.flow];
+				}
+				var inputVars = {};
+				for(var j in ctx._vars) {
+					inputVars[j] = ctx._vars[j];
+				}
+				inputVars[itemName] = array[i];
+				ctx.createFlowEngine(tempFlow).setInputVars(inputVars).execute(function(outputVars) {
+					if(typeof outputVars != 'undefined') {
+						for(var j in outputVars) {
+							ctx._vars[j] = outputVars[j];
+						}
+					}
+					setTimeout(checkNext, 1);
+				});
+			}
+			else {
+				setTimeout(next, 1);
+			}
+		}
+		setTimeout(checkNext, 1);
+	}
+	else {
+		setTimeout(next, 1);
+	}
+}
