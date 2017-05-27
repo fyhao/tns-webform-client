51f66e3e62444a1f1cb1ccecc16692aec02a1ed8
fyhao <fyhao1@gmail.com>
2017-05-27 14:40:02 +0800

    evaljs ctx._vars

--- app/lib/modBrowser/steps/evaljs.js
+++ app/lib/modBrowser/steps/evaljs.js
@@ -1,7 +1,7 @@
 module.exports = {
 	
 	process : function(ctx, step, next) {
-		var val = eval('vars = ' + JSON.stringify(vars) + '; ' + step.code);
+		var val = eval('vars = ' + JSON.stringify(ctx._vars) + '; ' + step.code);
 		ctx._vars[step.var] = val;
 		setTimeout(next, 1);
 	}
