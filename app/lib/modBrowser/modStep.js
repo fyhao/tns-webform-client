e7fbe5343645eb86d9b7283ad2228212159cdfc7
fyhao <fyhao1@gmail.com>
2017-05-27 11:16:39 +0800

    #46 step module

---
+++ app/lib/modBrowser/modStep.js
@@ -0,0 +1,85 @@
+// IMPORT
+import * as fs from "tns-core-modules/file-system";
+var util = require('./lib/util.js');
+
+module.exports = {
+	bootstrap : function() {
+		_bootstrap();
+	}
+	,
+	processStep : function(ctx, step, next) {
+		_processStep(ctx, step, next);
+	}
+	,
+	checkSpec : function(step) {
+		_checkSpec(step);
+	}
+};
+
+// public variable
+var stepDefinitions = [];
+
+// CONSTANTS
+var LIB_STEPS_PATH = "lib/steps";
+
+// REGION _bootstrap
+var _bootstrap = function() {
+	// scan available folders for step definition files
+	scanRootLevelFiles();
+	scanFolders();
+	scanNodeModules();
+}
+var scanRootLevelFiles = function() {
+	var list = fs.readdirSync(fs.path.join(__dirname, LIB_STEPS_PATH)); 
+	list.forEach(function(filename) {
+		if(filename.lastIndexOf('.js') > -1) {
+			var filepath = './' + LIB_STEPS_PATH + '/' + filename;
+			delete require.cache[require.resolve(filepath)]; // delete require cache
+			var def = require(filepath);
+			var name = filename.replace('.js', '');
+			if(typeof stepDefinitions[name] !== 'undefined') {
+				throw new Error('ERROR the step definition [' + name + '] exist');
+			}
+			stepDefinitions[name] = def;
+		}
+	});
+}
+var scanFolders = function() {
+	
+}
+var scanNodeModules = function() {
+	//TODO
+}
+// ENDREGION _bootstrap
+
+// REGION _processStep
+
+var _processStep = function(ctx, step, next) {
+	var pro = new StepProcessor(ctx, step, next);
+	pro.process();
+}
+var StepProcessor = function(ctx, step, next) {
+	this.ctx = ctx;
+	this.step = step;
+	
+	var def = null;
+	var spec = null;
+	var init = function() {
+		findDef();
+	}
+	var findDef = function() {
+		if(typeof stepDefinitions[step.type] !== 'undefined') {
+			def = stepDefinitions[step.type];
+		}
+	}
+	this.process = function() {
+		if(def === null) {
+			setTimeout(next, 1);
+			return;
+		}
+		def.process(ctx, step, next);
+	}
+	init();
+}
+
+// ENDREGION _processStep
