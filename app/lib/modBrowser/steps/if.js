a66c1a15b494fde244f2dad575d7d79d516385f7
fyhao <fyhao1@gmail.com>
2017-06-03 17:49:15 +0800

    #113 add expr

--- app/lib/modBrowser/steps/if.js
+++ app/lib/modBrowser/steps/if.js
@@ -3,20 +3,25 @@ module.exports = {
 	process : function(ctx, step, next) {
 		var val = ctx._vars[step.var];
 		var validated = false;
-		if(step.if == 'contains') {
-			validated = val.indexOf(step.pattern) != -1;
+		if(typeof step.expr !== 'undefined') {
+			validated = eval('vars = ' + JSON.stringify(ctx._vars) + '; ' + step.expr);
 		}
-		else if(step.if == 'equal') {
-			//console.log('execute if [' + val + '] = [' + step.pattern + ']');
-			validated = val == step.pattern;
-		}
-		else if(step.if == 'eq') {
-			//console.log('execute if [' + val + '] = [' + step.pattern + ']');
-			validated = val == step.pattern;
-		}
-		else if(step.if == 'neq') {
-			//console.log('execute if [' + val + '] = [' + step.pattern + ']');
-			validated = val != step.pattern;
+		else {
+			if(step.if == 'contains') {
+				validated = val.indexOf(step.pattern) != -1;
+			}
+			else if(step.if == 'equal') {
+				//console.log('execute if [' + val + '] = [' + step.pattern + ']');
+				validated = val == step.pattern;
+			}
+			else if(step.if == 'eq') {
+				//console.log('execute if [' + val + '] = [' + step.pattern + ']');
+				validated = val == step.pattern;
+			}
+			else if(step.if == 'neq') {
+				//console.log('execute if [' + val + '] = [' + step.pattern + ']');
+				validated = val != step.pattern;
+			}
 		}
 		if(validated) {
 			if(step.yes_subflow != null) {
