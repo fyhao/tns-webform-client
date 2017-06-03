79ccf0e267ec5f8e57c46e775383b15892a33f90
fyhao <fyhao1@gmail.com>
2017-06-03 20:38:40 +0800

    #71 imgshow init hook

--- app/lib/modBrowser/widgets/imgshow.js
+++ app/lib/modBrowser/widgets/imgshow.js
@@ -2,9 +2,13 @@ var util = require('../../../utils/MyUtil');
 module.exports = {
 	
 	renderWidget : function(param, fn) {
+		fn('<div id="' + param.id + '"></div>');
+	}
+	,
+	init : function(param, opts) {
 		var query = typeof(param.query) != 'undefined' ? param.query : '';
     	util.imgshow().load(query, function(html) {
-			fn(html);
+			opts.wv.ios.stringByEvaluatingJavaScriptFromString('document.getElementById("' + param.id + '").innerHTML = "' + html + '"');
 		});
 	}
 }
