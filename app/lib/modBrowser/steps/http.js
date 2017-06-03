57b2b0e96295172da09939a669a0b0a3128496a7
fyhao <fyhao1@gmail.com>
2017-06-03 18:10:34 +0800

    #66 errorFlow

--- app/lib/modBrowser/steps/http.js
+++ app/lib/modBrowser/steps/http.js
@@ -7,6 +7,13 @@ module.exports = {
 		};
 		if(typeof step.params !== 'undefined') frequestObj.params = step.params;
 		if(typeof step.headers !== 'undefined') frequestObj.headers = step.headers;
+		
+		if(typeof step.errorFlow !== 'undefined') {
+			frequestObj.error = function(err) {
+				callErrorFlow(step.errorFlow, err);
+			}
+		}
+		
 		if(typeof step.varJson !== 'undefined') {
 			frequestObj.callbackJSON = function(json) {
 				ctx._vars[step.varJson] = json;
@@ -27,3 +34,19 @@ module.exports = {
 	}
 }
 
+
+var callErrorFlow = function(flow, err) {
+	ctx._vars['http_error'] = err;
+	var inputVars = {};
+	for(var i in ctx._vars) {
+		inputVars[i] = ctx._vars[i];
+	}
+	ctx.createFlowEngine(flow).setInputVars(inputVars).execute(function(outputVars) {
+		if(typeof outputVars != 'undefined') {
+			for(var i in outputVars) {
+				ctx._vars[i] = outputVars[i];
+			}
+		}
+		setTimeout(next, 1);
+	});
+}
