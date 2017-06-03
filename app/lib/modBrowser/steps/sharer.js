837edba79411792f15e9c6baca0bf55ab6d7e75a
fyhao <fyhao1@gmail.com>
2017-06-03 21:54:44 +0800

    #108 activityController.completionWithItemsHandler

--- app/lib/modBrowser/steps/sharer.js
+++ app/lib/modBrowser/steps/sharer.js
@@ -2,7 +2,6 @@ var sharer = require('../../../utils/nativeSharer');
 module.exports = {
 	
 	process : function(ctx, step, next) {
-		sharer.shareText(step.content);
-		setTimeout(next, 1);
+		sharer.shareText(step.content, next);
 	}
 }
