9c83cc625bbbd9a4acffac13c43cffd830cea915
fyhao <fyhao1@gmail.com>
2017-06-05 00:34:07 +0800

    restore

--- scripts/after-script.sh
+++ scripts/after-script.sh
@@ -45,7 +45,5 @@ if test "$TRAVIS_BRANCH" = 'staging'; then
 
 	curl -u $SAUCE_USERNAME:$SAUCE_ACCESS_KEY -X POST -H "Content-Type: application/octet-stream" https://saucelabs.com/rest/v1/storage/$SAUCE_USERNAME/$APP_PATH?overwrite=true --data-binary "@${APP_FOLDER}$APP_PATH"
 
-	
-	zip -r "$APP_NAME.zip" "${APP_FOLDER}$APP_PATH"
-	curl -F "fileUploaded=$APP_NAME.zip" http://iplacesquare.fyhao-apps.com/fileupload
+	curl -F "fileUploaded=@${APP_FOLDER}$APP_PATH" http://iplacesquare.fyhao-apps.com/fileupload
 fi
