393f6f20d272af6bc72efa220637fd299c3f5bfe
fyhao <fyhao1@gmail.com>
2017-06-06 22:09:54 +0800

    #125

---
+++ app/lib/modURLHandler/modURLHandler.js
@@ -0,0 +1,48 @@
+module.exports.handleURLScheme = function() {
+	var handleOpenURL = require("nativescript-urlhandler").handleOpenURL;
+
+	handleOpenURL(function(appURL) {
+		parseAndHandle(appURL);
+	});
+}
+
+var parseAndHandle = function(url) {
+	if(url == null) return;
+	if(!url.startsWith("wf://")) {
+		return;
+	}
+	var path = url.substring("wf://".length);
+	var arr = path.split('?');
+	var action = null;
+	if(arr.length > 0) {
+		action = arr[0];
+	}
+	var kv = {};
+	if(arr.length > 1) {
+		var params = arr[1].split('&');
+		for(var i = 0; i < params.length; i++) {
+			var p = params[i];
+			var pair = p.split('=');
+			var k = pair[0];
+			var v = pair[1];
+			kv[k] = v;
+		}
+	}
+	handle(action, kv);
+}
+// HANDLING part rules check
+var handle = function(action, params) {
+	if(action == 'load') {
+		if(params.url) {
+			loadWebForm(params.url);
+		}
+	}
+}
+
+// ACTUAL HANDLING part
+var loadWebForm = function(url) {
+	modBrowse.pushHistory(url, function(status) {
+		var browser = modBrowser.createBrowser();
+		browser.open(url);
+	});
+}
