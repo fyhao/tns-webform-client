dd8af295cc67dbbce77df926d9bd71611f3e775c
fyhao <fyhao1@gmail.com>
2017-06-10 13:41:34 +0800

    #64 remove maximumAge
    This works on abd

--- app/utils/nativeGeolocation.js
+++ app/utils/nativeGeolocation.js
@@ -15,7 +15,7 @@ module.exports = {
 };
 
 var getLocation = function(fn) {
-	var location = geolocation.getCurrentLocation({desiredAccuracy: 3, updateDistance: 10, maximumAge: 20000, timeout: 20000}).
+	var location = geolocation.getCurrentLocation({desiredAccuracy: 3, updateDistance: 10, timeout: 20000}).
 		then(function(loc) {
 			fn(null, loc); //latitude longitude 
 		}, function(e){
