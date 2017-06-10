63908420b6537cd0964f0dfe51cba933dafb66f1
fyhao <fyhao1@gmail.com>
2017-06-10 12:52:01 +0800

    enable location

--- app/utils/nativeGeolocation.js
+++ app/utils/nativeGeolocation.js
@@ -1,6 +1,9 @@
 var geolocation = require("nativescript-geolocation");
 module.exports = {
 	getLocation: function(fn) {
+		if (!geolocation.isEnabled()) {
+			geolocation.enableLocationRequest();
+		}
 		var location = geolocation.getCurrentLocation({desiredAccuracy: 3, updateDistance: 10, maximumAge: 20000, timeout: 20000}).
 		then(function(loc) {
 			fn(null, loc); //latitude longitude 
