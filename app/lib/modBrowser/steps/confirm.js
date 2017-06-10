96672df9ed167ee59cef67c317bfd76d706c863e
fyhao <fyhao1@gmail.com>
2017-06-10 20:41:18 +0800

    #57 nativescript confirmation dialog

--- app/lib/modBrowser/steps/confirm.js
+++ app/lib/modBrowser/steps/confirm.js
@@ -1,60 +1,23 @@
+var dialogs = require("ui/dialogs");
 module.exports = {
 	
 	process : function(ctx, step, next) {
-		var dialog = Ti.UI.createAlertDialog({
-			cancel: 1,
-			buttonNames: step.buttons,
-			message: step.message,
-			title: step.title
-		  });
-		  dialog.addEventListener('click', function(e){
-			if(e.index == 0) {
-				if(step.yes_subflow != null) {
-					var tempFlow = null;
-					if(typeof ctx.flows != 'undefined') {
-						tempFlow = ctx.flows[step.yes_subflow];
-					}
-					var inputVars = {};
-					for(var i in ctx._vars) {
-						inputVars[i] = ctx._vars[i];
-					}
-					new FlowEngine(tempFlow).setContext(ctx).setInputVars(inputVars).execute(function(outputVars) {
-						if(typeof outputVars != 'undefined') {
-							for(var i in outputVars) {
-								ctx._vars[i] = outputVars[i];
-							}
-						}
-						setTimeout(next, 1);
-					});
-				}
-				else {
-					setTimeout(next, 1);
-				}
-			}
-			else {
-				if(step.no_subflow != null) {
-					var tempFlow = null;
-					if(typeof ctx.flows != 'undefined') {
-						tempFlow = ctx.flows[step.no_subflow];
-					}
-					var inputVars = {};
-					for(var i in ctx._vars) {
-						inputVars[i] = ctx._vars[i];
-					}
-					new FlowEngine(tempFlow).setContext(ctx).setInputVars(inputVars).execute(function(outputVars) {
-						if(typeof outputVars != 'undefined') {
-							for(var i in outputVars) {
-								ctx._vars[i] = outputVars[i];
-							}
-						}
-						setTimeout(next, 1);
-					});
-				}
-				else {
-					setTimeout(next, 1);
-				}
-			}
-		  });
-		  dialog.show();
+		var title = step.title ? step.title : 'Confirmation';
+		var message = step.message;
+		var okButtonText = step.okButtonText ? step.okButtonText : 'OK';
+		var cancelButtonText = step.cancelButtonText ? step.cancelButtonText : 'Cancel';
+		var neutralButtonText = step.neutralButtonText ? step.neutralButtonText : 'Neutral';
+		dialogs.confirm({
+			title: title,
+			message: message,
+			okButtonText: okButtonText,
+			cancelButtonText: cancelButtonText,
+			neutralButtonText: neutralButtonText
+		}).then(function (result) {
+			// result argument is boolean
+			console.log("Dialog result: " + result);
+			ctx.vars[step.result] = result;
+			setTimeout(next, 1);
+		});
 	}
 }
