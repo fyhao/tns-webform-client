b2a6278616cc82ac0b20fcc13490e48781fd63ce
fyhao <fyhao1@gmail.com>
2017-06-10 07:29:53 -0700

    Revert "#78 new step show hide to show/hide the widgets"
    This reverts commit 619f39cd59e4eb59e2918b6fb2fd021447031466.

--- app/lib/modBrowser/steps/show.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	
-	process : function(ctx, step, next) {
-		var visibility = 'visible';
-		ctx.wv.ios.stringByEvaluatingJavaScriptFromString('document.getElementById("' + step.name + '").style.visibility = "' + visibility + '"');
-		setTimeout(next, 1);
-	}
-}
