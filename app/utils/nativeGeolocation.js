c014005697b12db941e37cd6fa0e476ac20720ab
fyhao <fyhao1@gmail.com>
2017-06-10 13:12:14 +0800

    implement promise for enableLocationRequest

--- app/utils/nativeGeolocation.js
+++ app/utils/nativeGeolocation.js
@@ -2,14 +2,24 @@ var geolocation = require("nativescript-geolocation");
 module.exports = {
 	getLocation: function(fn) {
 		if (!geolocation.isEnabled()) {
-			geolocation.enableLocationRequest();
+			geolocation.enableLocationRequest().then(function() {
+				getLocation(fn);
+			}, function(err) {
+				alert("Error enableLocationRequest: " + err);
+			});
 		}
-		var location = geolocation.getCurrentLocation({desiredAccuracy: 3, updateDistance: 10, maximumAge: 20000, timeout: 20000}).
+		else {
+			getLocation(fn);
+		}
+	}
+};
+
+var getLocation = function(fn) {
+	var location = geolocation.getCurrentLocation({desiredAccuracy: 3, updateDistance: 10, maximumAge: 20000, timeout: 20000}).
 		then(function(loc) {
 			fn(null, loc); //latitude longitude 
 		}, function(e){
 			console.log("Error: " + e.message);
 			fn(e);
 		});
-	}
-};
+}
