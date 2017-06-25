a1fd2f8ba75c7c24a50faece3cfebc0b479af675
fyhao <fyhao1@gmail.com>
2017-06-25 05:45:31 -0700

    try catch

--- app/utils/nativeSpeech.js
+++ app/utils/nativeSpeech.js
@@ -1,25 +1,28 @@
 // text to speech and speech recognition
 
-module.exports = {}
+module.exports = {
 	speak : function(text) {
 		/// javascript
-		var TextToSpeech = require("nativescript-texttospeech");
+		try {
+			var TextToSpeech = require("nativescript-texttospeech");
 
-		var TNSTextToSpeech = TextToSpeech.TNSTextToSpeech;
-		var SpeakOptions = TextToSpeech.SpeakOptions;
-		
-		var TTS = new TNSTextToSpeech();
+			var TNSTextToSpeech = TextToSpeech.TNSTextToSpeech;
+			
+			var TTS = new TNSTextToSpeech();
 
-		var speakOptions: SpeakOptions = {
-			text: text, /// *** required ***
-			speakRate: 0.5 // optional - default is 1.0
-			pitch: 1.0 // optional - default is 1.0
-			volume: 1.0 // optional - default is 1.0
-			language: "en-GB"  // optional - default is system language,
-			finishedCallback: Function // optional
+			var speakOptions = {
+				text: text, /// *** required ***
+				speakRate: 0.5, // optional - default is 1.0
+				pitch: 1.0, // optional - default is 1.0
+				volume: 1.0, // optional - default is 1.0
+				language: "en-GB",  // optional - default is system language,
+				finishedCallback: function(){} // optional
+			}
+
+			// Call the `speak` method passing the SpeakOptions object
+			TTS.speak(speakOptions);
+		} catch (e) {
+			alert(e)
 		}
-
-		// Call the `speak` method passing the SpeakOptions object
-		TTS.speak(speakOptions);
 	}
 }
