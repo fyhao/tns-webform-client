e1d29d0169016ad563fc66cd3f9c9d79e623eab5
fyhao <fyhao1@gmail.com>
2017-06-29 23:15:40 +0800

    Revert "remove console log"
    This reverts commit a61aa43ac64f32e0fec8cc234d91ede710e8362d.

--- app/lib/modBrowser/steps/getValue.js
+++ app/lib/modBrowser/steps/getValue.js
@@ -3,6 +3,7 @@ module.exports = {
 	process : function(ctx, step, next) {
 		var name = step.name;
 		var value = ctx.wv.ios.stringByEvaluatingJavaScriptFromString('document.getElementById("' + name + '").value');
+		console.log('getValue ' + name + ' = ' + value)
 		ctx.vars[step.var] = value;
 		setTimeout(next, 1);
 	}
