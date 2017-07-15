f6e3b328ef488555c96b7f1c019b16a4fca489e0
fyhao <fyhao1@gmail.com>
2017-07-15 22:24:41 +0800

    #222 uploadFile use nativescript-background-http

--- app/lib/modBrowser/steps/uploadFile.js
+++ app/lib/modBrowser/steps/uploadFile.js
@@ -1,7 +1,29 @@
 module.exports = {
 	
 	process : function(ctx, step, next) {
-		alert(step.message);
+		var bghttp = require("nativescript-background-http");
+
+		var session = bghttp.session("image-upload");
+
+		var request = {
+			url: step.url,
+			method: "POST",
+			headers: {
+				"Content-Type": "application/octet-stream",
+				step.param: step.filename
+			},
+			description: "{ 'uploading': '" + step.filename + "' }"
+		};
+
+		var task = session.uploadFile(ctx.blobVars[step.blob], request);
+
+		task.on("progress", logEvent);
+		task.on("error", logEvent);
+		task.on("complete", logEvent);
+
+		function logEvent(e) {
+			console.log(e.eventName);
+		}
 		setTimeout(next, 1);
 	}
 }
