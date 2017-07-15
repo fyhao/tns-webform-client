5b2bc224c9393127dee8cf475b359f96402448d6
fyhao <fyhao1@gmail.com>
2017-07-15 22:01:13 +0800

    image source from image asset
    refer https://docs.nativescript.org/cookbook/image-source

--- app/lib/modBrowser/steps/sharer.js
+++ app/lib/modBrowser/steps/sharer.js
@@ -3,10 +3,15 @@ module.exports = {
 	
 	process : function(ctx, step, next) {
 		if(typeof step.blob != 'undefined') {
-			sharer.shareImage(ctx.blobVars[step.blob], next);
+			var imageSource = require("image-source");
+			var source = new imageSource.ImageSource();
+			source.fromAsset(ctx.blobVars[step.blob]).then(function(source) {
+				sharer.shareImage(source, next);
+			});
 		}
 		else {
 			sharer.shareText(step.content, next);
 		}
 	}
 }
+//https://docs.nativescript.org/cookbook/image-source
