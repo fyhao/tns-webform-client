f7231783b103846f5cf9ae0211a78ac7f16e68b8
fyhao <fyhao1@gmail.com>
2017-07-15 22:35:40 +0800

    save image asset into image source then save to a file for upload

--- app/lib/modBrowser/steps/uploadFile.js
+++ app/lib/modBrowser/steps/uploadFile.js
@@ -1,29 +1,39 @@
 module.exports = {
 	
 	process : function(ctx, step, next) {
-		var bghttp = require("nativescript-background-http");
+		var folder = fs.knownFolders.documents();
+		var path = fs.path.join(folder.path, "Test.png");
+		var imageSource = require("image-source");
+		var source = new imageSource.ImageSource();
+		source.fromAsset(ctx.blobVars[step.blob]).then(function(source) {
+			var saved = source.saveToFile(path, "png");
+			var bghttp = require("nativescript-background-http");
 
-		var session = bghttp.session("image-upload");
+			var session = bghttp.session("image-upload");
 
-		var request = {
-			url: step.url,
-			method: "POST",
-			headers: {
-				"Content-Type": "application/octet-stream"
-			},
-			description: "{ 'uploading': '" + step.filename + "' }"
-		};
-		request.headers[step.param] = step.filename;
+			var request = {
+				url: step.url,
+				method: "POST",
+				headers: {
+					"Content-Type": "application/octet-stream"
+				},
+				description: "{ 'uploading': '" + step.filename + "' }"
+			};
+			request.headers[step.param] = step.filename;
 
-		var task = session.uploadFile(ctx.blobVars[step.blob], request);
+			var task = session.uploadFile(path, request);
 
-		task.on("progress", logEvent);
-		task.on("error", logEvent);
-		task.on("complete", logEvent);
+			task.on("progress", logEvent);
+			task.on("error", logEvent);
+			task.on("complete", logEvent);
 
-		function logEvent(e) {
-			console.log(e.eventName);
-		}
+			function logEvent(e) {
+				console.log(e.eventName);
+			}
+		});
+		
+		
+		
 		setTimeout(next, 1);
 	}
 }
