1416fd20177bc51e7318bee7fc96edf90e51e35b
fyhao <fyhao1@gmail.com>
2017-08-10 21:59:47 +0800

    Revert "test official tns again"
    This reverts commit 5032b6b3e1593e34528a4689a8538b92328e3f6a.

--- scripts/script.sh
+++ scripts/script.sh
@@ -9,15 +9,15 @@ PROVISIONING_TYPE="${e[2]}"
 
 if test "$TRAVIS_BRANCH" = 'staging'; then
 	if test "$PROVISIONING_TYPE" = '0'; then
-		tns build ios --emulator
+		nativescript-cli/bin/tns build ios --emulator
 	fi
 
 	if test "$PROVISIONING_TYPE" = '1'; then
-		tns build ios --release --for-device --provision "$PROVISIONING_PROFILE" --teamId "$DEVELOPMENT_TEAM"
+		nativescript-cli/bin/tns build ios --release --for-device --provision "$PROVISIONING_PROFILE" --teamId "$DEVELOPMENT_TEAM"
 	fi
 
 	if test "$PROVISIONING_TYPE" = '2'; then
-		tns build ios --release --for-device --provision "$PROVISIONING_PROFILE" --teamId "$DEVELOPMENT_TEAM"
+		nativescript-cli/bin/tns build ios --release --for-device --provision "$PROVISIONING_PROFILE" --teamId "$DEVELOPMENT_TEAM"
 	fi
 
 fi
