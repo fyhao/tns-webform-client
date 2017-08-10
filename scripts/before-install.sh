949d43bcb9a56dc2cb9e20c8df2a5c243d26d6f7
fyhao <fyhao1@gmail.com>
2017-08-10 21:30:56 +0800

    test staging to load official tns directly

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -34,18 +34,10 @@ fi
 npm install -g npm@$NPM_VERSION
 
 if test "$TRAVIS_BRANCH" = 'staging'; then
-	if test "$PROVISIONING_TYPE" = '10'; then
+	
 		npm install -g nativescript@$TNS_VERSION
-	else
-		npm install grunt-cli -g
-		git clone https://github.com/fyhao/nativescript-cli
-		cd nativescript-cli
-		git submodule update --init
-		npm install
-		grunt
-		cd ../
-		nativescript-cli/bin/tns usage-reporting disable
-	fi
+		tns usage-reporting disable
+	
 fi
 npm install
 if test "$TRAVIS_BRANCH" = 'staging'; then
