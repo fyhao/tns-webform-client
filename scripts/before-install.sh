6d78526915e5bf89241c45cbbab363ea7caeef84
fyhao <fyhao1@gmail.com>
2017-08-11 20:33:18 +0800

    test latest nativescript via latest-0811

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -41,7 +41,9 @@ if test "$TRAVIS_BRANCH" = 'staging'; then
 		npm install grunt-cli -g
 		git clone https://github.com/fyhao/nativescript-cli
 		cd nativescript-cli
-		git checkout fyhao-stable-master-1
+		##git checkout fyhao-stable-master-1
+		##test latest branch 0811 works
+		git checkout latest-0811
 		git submodule update --init
 		npm install
 		grunt
