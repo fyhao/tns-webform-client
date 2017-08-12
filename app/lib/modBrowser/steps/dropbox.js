fbbaabc3fe5ebe82ca4b070d671d7a4b9fa9a4f3
fyhao <fyhao1@gmail.com>
2017-08-12 18:36:03 +0800

    action listFolder step.path

--- app/lib/modBrowser/steps/dropbox.js
+++ app/lib/modBrowser/steps/dropbox.js
@@ -4,12 +4,15 @@ module.exports = {
 	process : function(ctx, step, next) {
 		var client = new Dropbox.DropBoxClient(step.token);
 		// list folder
-		var path = '/';
-		console.log('dropbox listFolder path: ' + path);
-		client.listFolder(path, function (result) {
-            console.log('dropbox listFolder result: ');
-			console.log(result);
-		});
+		var action = step.action;
+		if(action == 'listFolder') {
+			var path = step.path;
+			console.log('dropbox listFolder path: ' + path);
+			client.listFolder(path, function (result) {
+				console.log('dropbox listFolder result: ');
+				console.log(result);
+			});
+		}
 		setTimeout(next, 1);
 	}
 }
