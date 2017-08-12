ebc736930f8ac16876fdb0b5ff730d8731480def
fyhao <fyhao1@gmail.com>
2017-08-12 22:39:10 +0800

    typo back

--- app/lib/modBrowser/modWebform.js
+++ app/lib/modBrowser/modWebform.js
@@ -92,7 +92,7 @@ var showItemWebform = function(item, opts) {
 					_funcs['showCategory'](json.redirectUrl);
 				}
 				if(json.closewin) {
-					helper.goBack();
+					helper.back();
 				}
 				
 				//#47 submit callback level
