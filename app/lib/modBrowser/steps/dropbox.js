ccb7b774bef36f0f381219008c13afa8ad765afc
fyhao <fyhao1@gmail.com>
2017-08-12 18:13:37 +0800

    add path

--- app/lib/modBrowser/steps/dropbox.js
+++ app/lib/modBrowser/steps/dropbox.js
@@ -4,6 +4,7 @@ module.exports = {
 	process : function(ctx, step, next) {
 		var client = new Dropbox.DropBoxClient(step.token);
 		// list folder
+		var path = '/';
 		console.log('dropbox listFolder path: ' + path);
 		client.listFolder(path, function (result) {
             console.log('dropbox listFolder result: ');
