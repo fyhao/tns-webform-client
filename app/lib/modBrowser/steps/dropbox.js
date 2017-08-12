fee770ea16cfd718c7ae2f592cf4d8f361c7c369
fyhao <fyhao1@gmail.com>
2017-08-12 18:52:19 +0800

    dropbox logs

--- app/lib/modBrowser/steps/dropbox.js
+++ app/lib/modBrowser/steps/dropbox.js
@@ -11,7 +11,21 @@ module.exports = {
 			client.listFolder(path, function (result) {
 				console.log('dropbox listFolder result: ');
 				console.dir(result);
+				if(result.entries && result.entries.length) {
+					for(var i = 0; i < result.entries.length; i++) {
+						console.log(result.entries[i]);
+					}
+				}
+				ctx.vars[step.result] = result;
 			});
+		}
+		else if(action == 'log') {
+			var result = ctx.vars[step.result];
+			if(result.entries && result.entries.length) {
+				for(var i = 0; i < result.entries.length; i++) {
+					console.log(result.entries[i]);
+				}
+			}
 		}
 		setTimeout(next, 1);
 	}
