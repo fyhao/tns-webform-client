68aca1b7d7980e3f080e59d3fd1e614e61aa4a14
fyhao <fyhao1@gmail.com>
2017-08-26 17:52:23 +0800

    basic and advanced feature test

--- test/testModOfflinePage.js
+++ test/testModOfflinePage.js
@@ -18,7 +18,7 @@ modOfflinePage.setSettingController({
 });
 describe('testModOfflinePage', function() {
   this.timeout(15000);
-  describe('#addPageInCategory', function() {
+  describe('#basic feature', function() {
 	it('add page in category', function(done) {
 		_setting = {};
 		modOfflinePage.addPageInCategory({list:[]},null, function(status) {
@@ -62,6 +62,23 @@ describe('testModOfflinePage', function() {
 			});
 		});
     });
+	it('findBy invalid', function(done) {
+		_setting = {};
+		modOfflinePage.addPageInCategory({list:[{type:'item',item:1}]},'http://google.com', function(status) {
+			assert.equal(status, 0);
+			var json = JSON.parse(_setting['OfflinePageDB']);
+			assert.equal(json.length, 1);
+			// actual test
+			assert.equal(json[0].cat.list[0].item, 1);
+			modOfflinePage.findBy('id','1234', function(item) {
+				assert.equal(item, null);
+				var json1 = JSON.parse(_setting['OfflinePageDB']);
+				assert.equal(json1.length, 1);
+				assert.equal(json1[0].cat.list[0].item, 1);
+				done();
+			});
+		});
+    });
 	it('findById', function(done) {
 		_setting = {};
 		modOfflinePage.addPageInCategory({list:[{type:'item',item:1}]},'http://google.com', function(status) {
@@ -71,6 +88,23 @@ describe('testModOfflinePage', function() {
 			// actual test
 			assert.equal(json[0].cat.list[0].item, 1);
 			modOfflinePage.findById(json[0].id, function(item) {
+				var json1 = JSON.parse(_setting['OfflinePageDB']);
+				assert.equal(json1.length, 1);
+				assert.equal(json1[0].cat.list[0].item, 1);
+				done();
+			});
+		});
+    });
+	it('findById invalid', function(done) {
+		_setting = {};
+		modOfflinePage.addPageInCategory({list:[{type:'item',item:1}]},'http://google.com', function(status) {
+			assert.equal(status, 0);
+			var json = JSON.parse(_setting['OfflinePageDB']);
+			assert.equal(json.length, 1);
+			// actual test
+			assert.equal(json[0].cat.list[0].item, 1);
+			modOfflinePage.findById('1234', function(item) {
+				assert.equal(item, null);
 				var json1 = JSON.parse(_setting['OfflinePageDB']);
 				assert.equal(json1.length, 1);
 				assert.equal(json1[0].cat.list[0].item, 1);
@@ -94,6 +128,23 @@ describe('testModOfflinePage', function() {
 			});
 		});
     });
+	it('findByTitle invalid', function(done) {
+		_setting = {};
+		modOfflinePage.addPageInCategory({list:[{type:'item',item:1}]},'http://google.com', function(status) {
+			assert.equal(status, 0);
+			var json = JSON.parse(_setting['OfflinePageDB']);
+			assert.equal(json.length, 1);
+			// actual test
+			assert.equal(json[0].cat.list[0].item, 1);
+			modOfflinePage.findByTitle('1234', function(item) {
+				assert.equal(item, null);
+				var json1 = JSON.parse(_setting['OfflinePageDB']);
+				assert.equal(json1.length, 1);
+				assert.equal(json1[0].cat.list[0].item, 1);
+				done();
+			});
+		});
+    });
 	it('deleteById', function(done) {
 		_setting = {};
 		modOfflinePage.addPageInCategory({list:[{type:'item',item:1}]},'http://google.com', function(status) {
@@ -102,9 +153,27 @@ describe('testModOfflinePage', function() {
 			assert.equal(json.length, 1);
 			// actual test
 			assert.equal(json[0].cat.list[0].item, 1);
-			modOfflinePage.deleteById(json[0].id, function(item) {
+			modOfflinePage.deleteById(json[0].id, function(status) {
+				assert.equal(status, 0);
 				var json1 = JSON.parse(_setting['OfflinePageDB']);
 				assert.equal(json1.length, 0);
+				done();
+			});
+		});
+    });
+	it('deleteById invalid', function(done) {
+		_setting = {};
+		modOfflinePage.addPageInCategory({list:[{type:'item',item:1}]},'http://google.com', function(status) {
+			assert.equal(status, 0);
+			var json = JSON.parse(_setting['OfflinePageDB']);
+			assert.equal(json.length, 1);
+			// actual test
+			assert.equal(json[0].cat.list[0].item, 1);
+			modOfflinePage.deleteById('1234', function(status) {
+				assert.equal(status, 1);
+				var json1 = JSON.parse(_setting['OfflinePageDB']);
+				assert.equal(json1.length, 1);
+				assert.equal(json1[0].cat.list[0].item, 1);
 				done();
 			});
 		});
@@ -123,6 +192,22 @@ describe('testModOfflinePage', function() {
 				var json1 = JSON.parse(_setting['OfflinePageDB']);
 				assert.equal(json1[0].cat.list[0].item, 2);
 				done();
+			});
+		});
+    });
+	it('findByAndSave invalid', function(done) {
+		_setting = {};
+		modOfflinePage.addPageInCategory({list:[{type:'item',item:1}]},'http://google.com', function(status) {
+			assert.equal(status, 0);
+			var json = JSON.parse(_setting['OfflinePageDB']);
+			assert.equal(json.length, 1);
+			// actual test
+			assert.equal(json[0].cat.list[0].item, 1);
+			modOfflinePage.findByAndSave('id','1234', 'save',function(item) {
+				assert.equal(item, null);
+				done();
+			}, function savefn() {
+				done(); // put here to test it will not call second time, haha
 			});
 		});
     });
@@ -170,6 +255,36 @@ describe('testModOfflinePage', function() {
 				assert.equal(status, 0);
 				var json1 = JSON.parse(_setting['OfflinePageDB']);
 				assert.equal(json1.length, 0);
+				done();
+			});
+		});
+    });
+  });
+  describe('#advanced feature', function() {
+	it('add page in category title should be unique', function(done) {
+		_setting = {};
+		modOfflinePage.addPageInCategory({list:[]},null, function(status) {
+			assert.equal(status, 0);
+			var json = JSON.parse(_setting['OfflinePageDB']);
+			assert.equal(json.length, 1);
+			var item = json[0];
+			assert.equal(item.sourceURL, null);
+			assert.equal(item.title, 'Untitled');
+			assert.equal(item.cat.list.length, 0);
+			modOfflinePage.addPageInCategory({list:[]},null, function(status) {
+				assert.equal(status, 0);
+				var json = JSON.parse(_setting['OfflinePageDB']);
+				assert.equal(json.length, 2);
+				var item = json[0];
+				assert.equal(item.sourceURL, null);
+				assert.equal(item.title, 'Untitled');
+				assert.equal(item.cat.list.length, 0);
+				
+				var title1 = json[0].title;
+				var title2 = json[1].title;
+				assert.equal(title1 != title2, true);
+				assert.equal(title1, 'Untitled');
+				assert.equal(title2, 'Untitled1');
 				done();
 			});
 		});
