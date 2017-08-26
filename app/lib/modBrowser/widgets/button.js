ec189fd7a12c861723bf4d11933ba07523cbfed1
fyhao <fyhao1@gmail.com>
2017-08-26 16:05:32 +0800

    #285 UI button styles enhancement with test

--- app/lib/modBrowser/widgets/button.js
+++ app/lib/modBrowser/widgets/button.js
@@ -5,18 +5,22 @@ module.exports = {
 			// multi buttons
 			var html = '';
 			var buttons = param.buttons;
+			var cols = param.cols || 3;
 			html += '<div>';
 			for(var i = 0; i < buttons.length; i++) {
 				var btn = buttons[i];
 				var visibility = typeof btn.visibility != 'undefined' && !btn.visibility ? 'hidden' : 'visible';
 				html += '<input type="button" id="' + btn.name + '" value="' + btn.title + '" style="visibility:' + visibility + '"/>';
+				if((i + 1) % cols == 0 && i < buttons.length - 1) {
+					html += '</div><div>';
+				}
 			}
 			html += '</div>';
 			fn(html);
 		}
 		else {
 			var visibility = typeof param.visibility != 'undefined' && !param.visibility ? 'hidden' : 'visible';
-			var html = '<input type="button" id="' + param.name + '" value="' + param.title + '" style="visibility:' + visibility + '"/>';
+			var html = '<input type="button" id="' + param.name + '" value="' + param.title + '" style="visibility:' + visibility + ';width:100%;"/>';
 			fn(html);
 		}
 	}
