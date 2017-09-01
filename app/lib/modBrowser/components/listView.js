ae5e9b9a722327bf48874d03866f4275ee9d997f
fyhao <fyhao1@gmail.com>
2017-09-01 23:06:02 +0800

    init listview

--- app/lib/modBrowser/components/listView.js
+++ app/lib/modBrowser/components/listView.js
@@ -3,6 +3,17 @@ module.exports = {
 	
 	process : function(c) {
 		c.comp = new listViewModule.ListView();
-		c.comp.src = c.src;
+		var listView = c.comp;
+		listView.items = cat.list;
+		listView.on(listViewModule.ListView.itemLoadingEvent, function (args1) {
+			if (!args1.view) {
+				// Create label if it is not already created.
+				args1.view = new labelModule.Label();
+				args1.view.height = 44;
+			}
+			args1.view.cat = cat.list[args1.index];
+			args1.view.text = cat.list[args1.index].title;
+
+		});
 	}
 }
