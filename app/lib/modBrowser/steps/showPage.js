06b951cbed4b1675ff1eb8835d457fff5b3ca343
fyhao <fyhao1@gmail.com>
2017-09-01 23:12:03 +0800

    off debug

--- app/lib/modBrowser/steps/showPage.js
+++ app/lib/modBrowser/steps/showPage.js
@@ -1,7 +1,7 @@
 module.exports = {
 	
 	process : function(ctx, step, next) {
-		console.log('In showPage');
+		//console.log('In showPage');
 		ctx.showItemNSPage(step.page);
 		setTimeout(next, 1);
 	}
