7c02a270f38d130e567bf7957d359041502fc0c9
fyhao <fyhao1@gmail.com>
2017-09-02 17:29:43 +0800

    itemPage.css

--- app/lib/modBrowser/modPage.js
+++ app/lib/modBrowser/modPage.js
@@ -22,7 +22,9 @@ var modWebform = require('./modWebform.js');
 function showItemNSPage(itemPage) {
 	//console.log('showItemNSPage: ' + JSON.stringify(itemPage));
 	var page = new pagesModule.Page();
-	page.addCss('button {background-color:red;}');
+	if(itemPage.css) {
+		page.addCss(itemPage.css);
+	}
 	itemPage.comp = page;
 	processComponents(itemPage);
 	helpers.navigate(function(){return page;});
