0462d14fcba4f0240316a78f47ab6f579712e43c
fyhao <fyhao1@gmail.com>
2017-09-02 16:42:37 +0800

    gridLayout process columns row

--- app/lib/modBrowser/components/gridLayout.js
+++ app/lib/modBrowser/components/gridLayout.js
@@ -3,5 +3,35 @@ module.exports = {
 	
 	process : function(c) {
 		c.comp = new GridLayout();
+		if(c.columns) {
+			var arr = c.columns.split(',');
+			for(var i in arr) {
+				var k = arr[i].trim();
+				if(k == '*') {
+					gridLayout.addColumn(new ItemSpec(1, 'star'));
+				}
+				else if(k == 'auto') {
+					gridLayout.addColumn(new ItemSpec(1, 'auto'));
+				}
+				else {
+					gridLayout.addColumn(new ItemSpec(parseInt(k), 'pixel'));
+				}
+			}
+		}
+		if(c.rows) {
+			var arr = c.rows.split(',');
+			for(var i in arr) {
+				var k = arr[i].trim();
+				if(k == '*') {
+					gridLayout.addRow(new ItemSpec(1, 'star'));
+				}
+				else if(k == 'auto') {
+					gridLayout.addRow(new ItemSpec(1, 'auto'));
+				}
+				else {
+					gridLayout.addRow(new ItemSpec(parseInt(k), 'pixel'));
+				}
+			}
+		}
 	}
 }
