fe3cbd4b6a527cbd70ef59913141d593d0a39d64
fyhao <fyhao1@gmail.com>
2017-09-02 17:37:41 +0800

    searchBar submit args.object.text

--- app/lib/modBrowser/components/searchBar.js
+++ app/lib/modBrowser/components/searchBar.js
@@ -5,7 +5,7 @@ module.exports = {
 		var ctx = c.ctx;
 		var searchBar = c.comp;
 		searchBar.on(SearchBar.submitEvent, function (args) { 
-			ctx.vars['_text'] = args.text;
+			ctx.vars['_text'] = args.object.text;
 			ctx.createFlowEngine(c.submit).execute(function() {});
 		});
 		searchBar.on(SearchBar.clearEvent, function (args) {
