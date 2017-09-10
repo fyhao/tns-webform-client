1b6ab3bc0ccda52e69a64d46a63fb12980d756c0
fyhao <fyhao1@gmail.com>
2017-09-10 11:15:25 +0800

    reorder test case

--- test/alltests.js
+++ test/alltests.js
@@ -1,9 +1,8 @@
 //require('./testModBrowse.js');
 //require('./testModBrowser.js');
 //require('./testTemp.js');
-
+require('./testEvalParser.js');
 require('./testModFlow.js');
 require('./testModHTMLRenderer.js');
 require('./testModOfflinePage.js');
 require('./testModPage.js');
-require('./testEvalParser.js');
