621e89050949388e196c0e32dc3ab1bed6b105ad
fyhao <fyhao1@gmail.com>
2017-09-10 11:39:17 +0800

    #304

--- app/lib/modBrowser/steps/showPage.js
+++ app/lib/modBrowser/steps/showPage.js
@@ -2,6 +2,13 @@ module.exports = {
 	
 	process : function(ctx, step, next) {
 		//console.log('In showPage');
+		var page = {};
+		if(typeof step.page == 'string') {
+			page = ctx.pages[step.page];
+		}
+		else {
+			page = step.page;
+		}
 		ctx.showItemNSPage(step.page);
 		setTimeout(next, 1);
 	}
