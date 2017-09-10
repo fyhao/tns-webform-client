b043b5e265b6d8a15dbac863881b4404d26dc414
fyhao <fyhao1@gmail.com>
2017-09-10 11:00:56 +0800

    reorder test file in alltests

--- test/alltests.js
+++ test/alltests.js
@@ -1,8 +1,9 @@
 //require('./testModBrowse.js');
 //require('./testModBrowser.js');
-require('./testModFlow.js');
-//require('./testModHTMLRenderer.js');
 //require('./testTemp.js');
-//require('./testModOfflinePage.js');
-//require('./testModPage.js');
+
+require('./testModFlow.js');
+require('./testModHTMLRenderer.js');
+require('./testModOfflinePage.js');
+require('./testModPage.js');
 require('./testEvalParser.js');
