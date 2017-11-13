198d0342325cc723c10ebafbbf199fa648dd8886
fyhao <fyhao1@gmail.com>
2017-11-13 15:06:41 +0800

    local staging remove double quote for identity

--- scripts/local-staging.sh
+++ scripts/local-staging.sh
@@ -1,13 +1,13 @@
 # local staging entry script
 
 export APP_NAME="tnswebformclient"
-export 'DEVELOPER_NAME="iPhone Distribution: Khor Yong Hao (C4F7EVGZVS)"'
+export 'DEVELOPER_NAME=iPhone Distribution: Khor Yong Hao (C4F7EVGZVS)'
 export PROFILE_NAME="tnswebformprofile"
 export NODE_VERSION="6" NPM_VERSION="3" TNS_VERSION="rc"
-export 'CODE_SIGN_IDENTITY="iPhone Distribution: Khor Yong Hao (C4F7EVGZVS)"'
+export 'CODE_SIGN_IDENTITY=iPhone Distribution: Khor Yong Hao (C4F7EVGZVS)'
 export PROVISIONING_PROFILE="b96261dd-8691-46d4-89f4-1cfb195e10a3"
 export DEVELOPMENT_TEAM="C4F7EVGZVS"
-export 'XCBUILD_SAFE_SH="/Users/travis/build/fyhao/tns-webform-client/scripts/xcbuild-safe.sh"'
+export 'XCBUILD_SAFE_SH="/Users/user903758/git/tns-webform-client/scripts/xcbuild-safe.sh"'
 
 export TRAVIS_BRANCH="staging"
 
@@ -52,8 +52,4 @@ echo "Travis script"
 
 #after_script
 echo "Travis after_script"
-./scripts/after-script.sh
-
-#after_success
-echo "Travis after_success"
-./scripts/after-success.sh
+./scripts/remove-key.sh
