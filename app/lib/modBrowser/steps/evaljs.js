caa8522064657d93460fd077f1c768fd43bb6ec5
fyhao <fyhao1@gmail.com>
2018-02-05 22:08:54 +0800

    #376 include ctx, next variable into evaljs

--- app/lib/modBrowser/steps/evaljs.js
+++ app/lib/modBrowser/steps/evaljs.js
@@ -4,8 +4,8 @@ module.exports = {
 		if(typeof step.var != 'undefined' && step.code.indexOf('return') == -1) {
 			step.code = 'return ' + step.code;
 		}
-		var val = new Function('vars', 'vars; ' + step.code);
-		ctx.vars[step.var] = val(ctx.vars);
+		var val = new Function('vars', 'ctx', 'next', 'vars; ctx; next; ' + step.code);
+		ctx.vars[step.var] = val(ctx.vars, ctx, next);
 		setTimeout(next, global.STEP_TIMEOUT);
 	}
 }
