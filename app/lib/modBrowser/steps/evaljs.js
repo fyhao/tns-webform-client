ef436f683eb203278d2d04f9d1d474abd9d7938e
fyhao <fyhao1@gmail.com>
2018-02-05 22:18:58 +0800

    #376 add util into evaljs

--- app/lib/modBrowser/steps/evaljs.js
+++ app/lib/modBrowser/steps/evaljs.js
@@ -1,11 +1,12 @@
+var util = require('../../../utils/MyUtil');
 module.exports = {
 	
 	process : function(ctx, step, next) {
 		if(typeof step.var != 'undefined' && step.code.indexOf('return') == -1) {
 			step.code = 'return ' + step.code;
 		}
-		var val = new Function('vars', 'ctx', 'next', 'vars; ctx; next; ' + step.code);
-		ctx.vars[step.var] = val(ctx.vars, ctx, next);
+		var val = new Function('vars', 'ctx', 'util', 'next', 'vars; ctx; util; next; ' + step.code);
+		ctx.vars[step.var] = val(ctx.vars, ctx, util, next);
 		setTimeout(next, global.STEP_TIMEOUT);
 	}
 }
