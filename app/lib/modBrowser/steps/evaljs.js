3f268b9a240baf95a292d45bf10a856d2d7d08e5
fyhao <fyhao1@gmail.com>
2018-02-05 22:43:18 +0800

    #376 add timeout to evaljs mean will use next in our own code

--- app/lib/modBrowser/steps/evaljs.js
+++ app/lib/modBrowser/steps/evaljs.js
@@ -5,8 +5,20 @@ module.exports = {
 		if(typeof step.var != 'undefined' && step.code.indexOf('return') == -1) {
 			step.code = 'return ' + step.code;
 		}
+		var isNextWrapperCalled = false;
+		var nextWrapper = function() {
+			if(isNextWrapperCalled) return;
+			setTimeout(next, global.STEP_TIMEOUT);
+			isNextWrapperCalled = true;
+		}
+		
 		var val = new Function('vars', 'ctx', 'util', 'next', 'vars; ctx; util; next; ' + step.code);
-		ctx.vars[step.var] = val(ctx.vars, ctx, util, next);
-		setTimeout(next, global.STEP_TIMEOUT);
+		ctx.vars[step.var] = val(ctx.vars, ctx, util, nextWrapper);
+		if(typeof step.timeout != 'undefined') {
+			setTimeout(nextWrapper, step.timeout);
+		}
+		else {
+			setTimeout(next, global.STEP_TIMEOUT);
+		}
 	}
 }
