bbc531820d532128793c763c69df1fbab90cf286
fyhao <fyhao1@gmail.com>
2018-02-09 16:39:29 +0800

    showOptionDialog to support specify id and other custom object

--- app/lib/modBrowser/steps/showOptionDialog.js
+++ app/lib/modBrowser/steps/showOptionDialog.js
@@ -4,9 +4,15 @@ module.exports = {
 	process : function(ctx, step, next) {
 		var options = [];
 		for(var i = 0; i < step.options.length; i++) {
-			var opt = {};
-			opt.text = step.options[i];
-			options.push(opt);
+			var option = step.options;
+			if(typeof option == 'string') {
+				var opt = {};
+				opt.text = step.options[i];
+				options.push(opt);
+			}
+			else {
+				options.push(option);
+			}
 		}
 		util.showOptionDialog(options, {doneResult:function(result, selectedOption) {
 			ctx.vars[step.result] = result;
