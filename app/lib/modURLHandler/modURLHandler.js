a6c89dff456b69de692be5ccad891f63f4ace77d
fyhao <fyhao1@gmail.com>
2019-09-29 18:13:50 +0800

    Revert "Dev"

--- app/lib/modURLHandler/modURLHandler.js
+++ app/lib/modURLHandler/modURLHandler.js
@@ -1,16 +1,9 @@
-var modBrowse = require('../modBrowse/modBrowse.js');
-var logs = [];
 module.exports.handleURLScheme = function() {
 	var handleOpenURL = require("nativescript-urlhandler").handleOpenURL;
-	try {
-		logs.push('1');
-		handleOpenURL(function(appURL) {
-			logs.push('2');
-			parseAndHandle(appURL);
-		});
-	} catch (e) {
-		alert(e + " - " + JSON.stringify(logs));
-	}
+
+	handleOpenURL(function(appURL) {
+		parseAndHandle(appURL);
+	});
 }
 
 var parseAndHandle = function(url) {
@@ -18,7 +11,6 @@ var parseAndHandle = function(url) {
 	if(!url.startsWith("wf://")) {
 		return;
 	}
-	logs.push('2.1:' + url);
 	var path = url.substring("wf://".length);
 	var arr = path.split('?');
 	var action = null;
@@ -36,16 +28,12 @@ var parseAndHandle = function(url) {
 			kv[k] = v;
 		}
 	}
-	logs.push('2.2:' + action + ',' + JSON.stringify(kv));
 	handle(action, kv);
 }
 // HANDLING part rules check
 var handle = function(action, params) {
-	logs.push('2.3:' + action);
 	if(action == 'load') {
-		logs.push('2.4:' + params.url);
 		if(params.url) {
-			logs.push('2.5');
 			loadWebForm(params.url);
 		}
 	}
@@ -53,7 +41,5 @@ var handle = function(action, params) {
 
 // ACTUAL HANDLING part
 var loadWebForm = function(url) {
-	logs.push('2.6');
 	modBrowse.browseURL(url);
-	logs.push('2.7');
 }
